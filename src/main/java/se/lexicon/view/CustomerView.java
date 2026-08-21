7d04870d18caafe036136aa1b0fa70950a563d33
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-08-21 19:39:07 +0200

    creating CustomerView class

---
+++ src/main/java/se/lexicon/view/CustomerView.java
@@ -0,0 +1,25 @@
+package se.lexicon.view;
+
+public class CustomerView {
+
+    /* --- Create a new customer based on the information provided by the user --- */
+    public Customer getCustomerInput(){
+
+        String name = IO.readln("Name: ");
+        String email = IO.readln("Email: ");
+        String phone = IO.readln("Phone: ");
+        String type = IO.readln("Customer type (COMPANY/INDIVIDUAL): ");
+        String orgaNumber = IO.readln("Organization number (optional): ");
+
+        return new Customer(name, email, phone, CustomerType.valueOf(type.toUppercase()), orgaNumber.isBlank() ? null : orgaNumber);
+    }
+
+    /* --- Displays all the existing customers --- */
+    public void displayCustomers(List<Customer> customers){
+        if(customers.isEmpty()){
+            IO.println("❌ No customers found.");
+            return;
+        }
+        customers.forEach(IO::println);
+    }
+}

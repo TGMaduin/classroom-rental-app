e77732ff6d7dc164174c6c8eb3761c78c55fcd40
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-08-21 23:41:05 +0200

    updating the string format

--- src/main/java/se/lexicon/view/ConsoleMenu.java
+++ src/main/java/se/lexicon/view/ConsoleMenu.java
@@ -3,21 +3,22 @@ package se.lexicon.view;
 public class ConsoleMenu {
 
     public void displayMenu(){
-        IO.println(String.format(
-                "=============================",
-                "|    CLASSROOM RENTAL APP    |",
-                "=============================",
-                "1. Register customer",
-                "2. Add a booking user",
-                "3. Add a classroom",
-                "4. Show all customers",
-                "5. Show all classrooms",
-                "6. Search available classrooms",
-                "7. Create a booking",
-                "8. View classroom bookings",
-                "9. View customer bookings",
-                "10. View all upcoming bookings",
-                "11. Exit"
+        IO.println(String.format("""
+                =============================
+                |    CLASSROOM RENTAL APP    |
+                =============================
+                1. Register customer
+                2. Add a booking user
+                3. Add a classroom
+                4. Show all customers
+                5. Show all classrooms
+                6. Search available classrooms
+                7. Create a booking
+                8. View classroom bookings
+                9. View customer bookings
+                10. View all upcoming bookings
+                11. Exit
+                """
         ));
     }
 

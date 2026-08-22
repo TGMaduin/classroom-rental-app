fb7b4675c5aa7f1308b325d279d8e036c53a7ef5
TGMaduin <tgmaduin@gmail.com>
2026-08-22 09:53:43 +0200

    feat: define DAO interfaces

--- src/main/java/se/lexicon/dao/ClassroomDao.java
+++ src/main/java/se/lexicon/dao/ClassroomDao.java
@@ -1,4 +1,15 @@
 package se.lexicon.dao;
 
+import se.lexicon.model.Classroom;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClassroomDao {
+
+    Classroom save(Classroom classroom);
+
+    Optional<Classroom> findById(int id);
+
+    List<Classroom> findAll();
 }
